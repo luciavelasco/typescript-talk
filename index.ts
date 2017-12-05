2d1f38ecbc63eb661ff3ebfa73a96e7c11070f28
Lucia Velasco <luciakvelasco@gmail.com>
2017-12-05 00:12:47 +0000

    Last commit: todos and shit

--- index.ts
+++ index.ts
@@ -5,6 +5,25 @@ import { Chicken } from './src/chicken'
 
 console.log('Hello World!');
 
+/**
+ * 1. Setup
+ * 2. Different types and how to use them
+ * 3. Interfaces
+ * 4. Generics
+ * 5. JS vs ES6 vs TS
+ */
+
+
+// Typings
+// any, array, typles, void, enum, type, null, undefined, never
+// Typings and naming as good documentation
+// types helping you write by telling you what's next and what's available
+
+// Generics
+
+// Compare how-to in JS/ES5 vs ES6/2015 vs TS
+
+
 // Example of static type checking
 // let foo = 123;
 // foo = '456';
@@ -16,4 +35,12 @@ console.log(`${chicken.speak()}`);
 // console.log(`${chicken.speak()} at ${moment().format('h:mm:ssa')}`);
 console.log(chicken.getInfo());
 
-// todo: async/await
+
+
+function grabOne<T>(options: T[]): T {
+  return options[Math.floor(Math.random() * options.length)];
+}
+
+type TFruit = 'banana' | 'orange' | 'apple'
+const fruitBowl: TFruit[] = ['banana', 'banana', 'orange', 'apple', 'apple', 'apple']
+console.log(grabOne<TFruit>(fruitBowl))
